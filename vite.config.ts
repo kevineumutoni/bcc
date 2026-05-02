d63b2c6c945b4839a46d60e2bd104d9b0cccecfb
Kevine Umutoni <kevineumutonibugingo@gmail.com>
2026-05-02 19:41:50 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173, proxy: {} }
+  server: { port: 5173, proxy: {} },
+  optimizeDeps: {
+    include: ['leaflet'],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          leaflet: ['leaflet'],
+        },
+      },
+    },
+  },
 })
